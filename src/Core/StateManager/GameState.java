023a1c64047f6b9509a8121b9cb09b0569620c16
zerodivided714 <zerodivided714@localhost>
2013-08-06 19:56:55 -0400

    Added starting GameState

--- src/Core/StateManager/GameState.java
+++ src/Core/StateManager/GameState.java
@@ -1,5 +1,12 @@
 package Core.StateManager;
 
 public interface GameState {
+	
+	public void update();
+	public void draw();
+	public void load();
+	public void pause();
+	public void save();
+	public void dispose();
 
 }
---
+++ src/GameStates/GameStart.java
@@ -0,0 +1,43 @@
+package GameStates;
+
+import Core.StateManager.GameState;
+
+public class GameStart implements GameState {
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void load() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void save() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
