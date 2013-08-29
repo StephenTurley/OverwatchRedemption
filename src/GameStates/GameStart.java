36403c32d6c406dc95ef1ab29f8de7b434691cc8
zerodivided714 <zerodivided714@localhost>
2013-08-28 21:23:52 -0400

    Added keyboard input to move quad in the GameStart state.

--- src/GameStates/GameStart.java
+++ src/GameStates/GameStart.java
@@ -2,15 +2,24 @@ package GameStates;
 
 import static org.lwjgl.opengl.GL11.*;
 
+import java.util.HashMap;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
+
 import Core.Debug;
 import Core.StateManager.GameState;
 
 public class GameStart implements GameState {
+	
+	private float xDelta = 0;
+	private float yDelta = 0;
+	private final float VELOCITY = 5;
+	private HashMap<String, Boolean> flags = new HashMap<String, Boolean>();
 
 	@Override
 	public void update() {
-		// TODO Auto-generated method stub
-
+		handleInput();
+		moveQuad();
 	}
 
 	@Override
@@ -27,10 +36,10 @@ public class GameStart implements GameState {
 
 		// draw quad
 		glBegin(GL_QUADS);
-			glVertex2f(10,10);
-			glVertex2f(100+200,100);
-			glVertex2f(100+200,100+200);
-			glVertex2f(100,100+200);
+			glVertex2f(100 + xDelta,100 + yDelta);
+			glVertex2f(300 + xDelta,100 + yDelta);
+			glVertex2f(300 + xDelta,300 + yDelta);
+			glVertex2f(100 + xDelta,300 + yDelta);
 		glEnd();
 	}
 
@@ -55,18 +64,93 @@ public class GameStart implements GameState {
 	@Override
 	public void enter()
 	{
+		flags.put("up", false);
+		flags.put("down", false);
+		flags.put("left", false);
+		flags.put("right", false);
+		
+		
 		Debug.Trace("Start State has been entered!");
 		
 		glMatrixMode(GL_PROJECTION);
 		glLoadIdentity();
 		glOrtho(0, 800, 0, 600, 1, -1);
 		glMatrixMode(GL_MODELVIEW);
+		
 	}
 	
 	@Override
 	public void exit() {
 		// TODO Auto-generated method stub
 
+	}
+	
+	private void moveQuad()
+	{
+		if (flags.get("up"))
+		{
+			yDelta += VELOCITY;
+		}
+		if (flags.get("down"))
+		{
+			yDelta -= VELOCITY;
+		}
+		if (flags.get("left"))
+		{
+			xDelta -= VELOCITY;
+		}
+		if (flags.get("right"))
+		{
+			xDelta += VELOCITY;
+		}
+	}
+	
+	private void handleInput(){
+		while(Keyboard.next())
+		{
+			if(Keyboard.getEventKeyState() || Keyboard.isRepeatEvent())
+			{
+				if(Keyboard.getEventKey() == Keyboard.KEY_ESCAPE)
+				{
+					Display.destroy();
+					System.exit(0);
+				}
+				if(Keyboard.getEventKey() == Keyboard.KEY_A)
+				{
+					flags.put("left", true);
+				}
+				if(Keyboard.getEventKey() == Keyboard.KEY_D)
+				{
+					flags.put("right", true);
+				}
+				if(Keyboard.getEventKey() == Keyboard.KEY_W)
+				{
+					flags.put("up", true);
+				}
+				if(Keyboard.getEventKey() == Keyboard.KEY_S)
+				{
+					flags.put("down", true);
+				}
+			}else
+			{
+				if(Keyboard.getEventKey() == Keyboard.KEY_A)
+				{
+					flags.put("left", false);
+				}
+				if(Keyboard.getEventKey() == Keyboard.KEY_D)
+				{
+					flags.put("right", false);
+				}
+				if(Keyboard.getEventKey() == Keyboard.KEY_W)
+				{
+					flags.put("up", false);
+				}
+				if(Keyboard.getEventKey() == Keyboard.KEY_S)
+				{
+					flags.put("down", false);
+				}
+			}
+		}
 	}
 
 }
