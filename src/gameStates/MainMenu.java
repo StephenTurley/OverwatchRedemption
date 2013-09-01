5ccde57c1b81181d6ab021d29dc0d37b4e5a7bf6
zerodivided714 <zerodivided714@localhost>
2013-09-01 17:20:35 -0400

    Added a Lobby Gamestate Forgot to add Mainmenu and Movement test to the repo.. fixed that =D

---
+++ src/gameStates/MainMenu.java
@@ -0,0 +1,55 @@
+package gameStates;
+
+import core.stateManager.GameState;
+import core.stateManager.StateManager;
+
+public class MainMenu extends GameState {
+
+	public MainMenu(StateManager sm) {
+		super(sm);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update(int delta) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void save() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void enter() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void exit() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
