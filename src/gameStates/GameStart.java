fdd25a49608be080feb2ee441344b22563154f04
zerodivided714 <zerodivided714@localhost>
2013-09-01 17:11:33 -0400

    Added MainMenu and MovementTest GameStates Moved original GameStart code to the MovementTest class.

--- src/gameStates/GameStart.java
+++ src/gameStates/GameStart.java
@@ -1,211 +1,58 @@
 package gameStates;
 
-import static org.lwjgl.opengl.GL11.*;
+import core.stateManager.*;
 
-import java.util.HashMap;
-import org.lwjgl.input.Keyboard;
-import org.lwjgl.util.vector.Vector2f;
-
-import core.Debug;
-import core.Game;
-import core.stateManager.GameState;
-import core.stateManager.StateManager;
-
-import net.java.games.input.*;
 
 public class GameStart extends GameState {
 	
-	private float xVector = 0;
-	private float yVector = 0;
-	private float deadZone;
-	private final float VELOCITY = 1;
-	private HashMap<String, Boolean> flags = new HashMap<String, Boolean>();
-	private Controller gamepad;
 	
 	public GameStart(StateManager sm)
 	{
 		super(sm);
 	}
 
-
+	@Override
 	public void update(int delta) {
+		// TODO Auto-generated method stub
 		
-		handleInput(delta);
-		moveQuad(delta);
 	}
 
-
+	@Override
 	public void draw() {
-		// Clear the screen and depth buffer
-	
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
-
-		 
-
-		// set the color of the quad (R,G,B,A)
-	
-		glColor3f(0.5f,0.5f,1.0f);
-
-		// draw quad
-		glBegin(GL_QUADS);
-			glVertex2f(100 + xVector,100 + yVector);
-			glVertex2f(300 + xVector,100 + yVector);
-			glVertex2f(300 + xVector,300 + yVector);
-			glVertex2f(100 + xVector,300 + yVector);
-		glEnd();
+		// TODO Auto-generated method stub
+		
 	}
 
-
+	@Override
 	public void resume() {
 		// TODO Auto-generated method stub
-
+		
 	}
 
-
+	@Override
 	public void pause() {
 		// TODO Auto-generated method stub
-
+		
 	}
 
-
+	@Override
 	public void save() {
 		// TODO Auto-generated method stub
+		
+	}
 
-	}
-	
-	public void enter()
-	{
-		deadZone = Game.getGameConfig().getJoyStickDeadZone();
-		
-		flags.put("up", false);
-		flags.put("down", false);
-		flags.put("left", false);
-		flags.put("right", false);
-		
-		for (Controller c : ControllerEnvironment.getDefaultEnvironment().getControllers())
-		{
-			if(c.getType() == Controller.Type.GAMEPAD)
-			{
-				gamepad = c;
-				if(Game.getGameConfig().isDebugLogging())
-				{
-					Debug.Trace("Gamepad Detected: " + gamepad.getName());
-				}
-			}
-		}
-		
-		Debug.Trace("Start State has been entered!");
-		
-		glMatrixMode(GL_PROJECTION);
-		glLoadIdentity();
-		glOrtho(0, 800, 0, 600, 1, -1);
-		glMatrixMode(GL_MODELVIEW);
+	@Override
+	public void enter() {
+		// TODO Auto-generated method stub
 		
 	}
-	
+
+	@Override
 	public void exit() {
 		// TODO Auto-generated method stub
+		
+	}
 
-	}
 	
-	private void moveQuad(int delta)
-	{
-		if (flags.get("up"))
-		{
-			yVector += VELOCITY * delta;
-		}
-		if (flags.get("down"))
-		{
-			yVector -= VELOCITY * delta;
-		}
-		if (flags.get("left"))
-		{
-			xVector -= VELOCITY * delta;
-		}
-		if (flags.get("right"))
-		{
-			xVector += VELOCITY * delta;
-		}
-	}
-	
-	private void handleInput(int delta){
-		
-		if(gamepad != null)
-		{
-			gamepad.poll();
-			float x = 0;
-			float y = 0;
-			for(Component c : gamepad.getComponents())
-			{
-				//System.out.println(c.getName()+" : "+ c.getPollData());
-				if(c.getName().equals("x"))
-				{	
-					x = c.getPollData();
-				}
-				if(c.getName().equals("y"))
-				{
-					y=c.getPollData();
-				}
-				if(c.getName().equals("Select")||c.getName().equals("Unknown"))
-				{
-					if(c.getPollData() == 1.0)
-					{
-						super.sm.pop();
-					}
-				}
-			}
-			Vector2f leftStickInput = new Vector2f(x,y);
-			if(leftStickInput.lengthSquared() > deadZone)
-			{
-				xVector += VELOCITY * delta * leftStickInput.x;
-				yVector += VELOCITY * delta * leftStickInput.y * -1;
-			}
-		}
-		
-		while(Keyboard.next())
-		{
-			if(Keyboard.getEventKeyState() || Keyboard.isRepeatEvent())
-			{
-				if(Keyboard.getEventKey() == Keyboard.KEY_ESCAPE)
-				{
-					super.sm.pop();
-				}
-				if(Keyboard.getEventKey() == Keyboard.KEY_A)
-				{
-					flags.put("left", true);
-				}
-				if(Keyboard.getEventKey() == Keyboard.KEY_D)
-				{
-					flags.put("right", true);
-				}
-				if(Keyboard.getEventKey() == Keyboard.KEY_W)
-				{
-					flags.put("up", true);
-				}
-				if(Keyboard.getEventKey() == Keyboard.KEY_S)
-				{
-					flags.put("down", true);
-				}
-			}else
-			{
-				if(Keyboard.getEventKey() == Keyboard.KEY_A)
-				{
-					flags.put("left", false);
-				}
-				if(Keyboard.getEventKey() == Keyboard.KEY_D)
-				{
-					flags.put("right", false);
-				}
-				if(Keyboard.getEventKey() == Keyboard.KEY_W)
-				{
-					flags.put("up", false);
-				}
-				if(Keyboard.getEventKey() == Keyboard.KEY_S)
-				{
-					flags.put("down", false);
-				}
-			}
-		}
-	}
 
 }
