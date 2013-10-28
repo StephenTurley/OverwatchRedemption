83158476f316048ac8ec87c48ab22295f6f267a3
zerodivided714 <zerodivided714@localhost>
2013-10-28 00:24:23 -0400

    JoinGame now has a UI

--- src/gameStates/JoinGame.java
+++ src/gameStates/JoinGame.java
@@ -1,9 +1,103 @@
 package gameStates;
 
+import org.lwjgl.opengl.Display;
+
+import core.Debug;
+import core.Game;
 import core.stateManager.GameState;
 import core.stateManager.StateManager;
+import de.matthiasmann.twl.Button;
+import de.matthiasmann.twl.EditField;
+import de.matthiasmann.twl.GUI;
+import de.matthiasmann.twl.Label;
+import de.matthiasmann.twl.Widget;
+import de.matthiasmann.twl.renderer.lwjgl.LWJGLRenderer;
+import de.matthiasmann.twl.theme.ThemeManager;
 
 public class JoinGame extends GameState {
+	private LWJGLRenderer renderer; 
+    private UI uiWidget;
+    private GUI gui;
+	
+	private class UI extends Widget
+	{
+		private Button joinGameBtn;
+		
+		private EditField playerNameEdf;
+		private EditField hostNameEdf;
+		private EditField hostPortEdf;
+		
+		private Label playerNameLbl;
+		private Label hostNameLbl;
+		
+		
+		public UI()
+		{
+			joinGameBtn = new Button();
+			joinGameBtn.setTheme("button");
+			joinGameBtn.setText("Join Game");
+			joinGameBtn.addCallback(new Runnable(){
+				public void run()
+				{
+					//startServer();
+				}
+			});
+			add(joinGameBtn);
+			
+			playerNameEdf = new EditField();
+			playerNameEdf.setTheme("editField");
+			playerNameEdf.setText("Player 1");
+			add(playerNameEdf);
+			
+			hostNameEdf = new EditField();
+			hostNameEdf.setTheme("editField");
+			add(hostNameEdf);
+			
+			hostPortEdf = new EditField();
+			hostPortEdf.setTheme("editField");
+			hostPortEdf.setText(String.valueOf(Game.getGameConfig().getServerTCP()));
+			add(hostPortEdf);
+			
+			playerNameLbl = new Label("Player's Name:");
+			playerNameLbl.setTheme("label");
+			add(playerNameLbl);
+			
+			hostNameLbl = new Label("Hostname:Port :");
+			hostNameLbl.setTheme("label");
+			add(hostNameLbl);
+					
+		}
+		@Override
+		protected void layout() {
+			int width = Display.getWidth();
+			int height = Display.getHeight();
+			
+			final int gap = 25;
+			
+			joinGameBtn.adjustSize();
+			joinGameBtn.setPosition((width - joinGameBtn.getWidth()) - 50, height - 150);
+			
+			playerNameLbl.adjustSize();
+			playerNameLbl.setPosition(50, 100);
+			
+			playerNameEdf.setSize(125, 25);
+			playerNameEdf.setPosition(playerNameLbl.getX() + playerNameLbl.getWidth() + gap, 100);
+			
+			hostNameLbl.adjustSize();
+			hostNameLbl.setPosition(playerNameEdf.getX() + playerNameEdf.getWidth() + gap, 100);
+			
+			hostNameEdf.setSize(300, 25);
+			hostNameEdf.setPosition(hostNameLbl.getX() + hostNameLbl.getWidth() + gap, 100);
+			
+			hostPortEdf.setSize(50, 25);
+			hostPortEdf.setPosition(hostNameEdf.getX() + hostNameEdf.getWidth() + 10, 100);
+			
+		}
+		public String getPlayerName()
+		{
+			return playerNameEdf.getText();
+		}
+	}
 
 	public JoinGame(StateManager sm) {
 		super(sm);
@@ -23,7 +117,7 @@ public class JoinGame extends GameState {
 
 	@Override
 	public void draw() {
-		// TODO Auto-generated method stub
+		gui.update();
 
 	}
 
@@ -47,8 +141,24 @@ public class JoinGame extends GameState {
 
 	@Override
 	public void enter() {
-		// TODO Auto-generated method stub
-
+		try
+		{
+			//set up gui
+			renderer = new LWJGLRenderer();
+			uiWidget = new UI();
+			gui = new GUI(uiWidget, renderer);
+			ThemeManager theme = ThemeManager.createThemeManager(
+	                UI.class.getResource("/gui/HostGameTheme.xml"), renderer);
+	        gui.applyTheme(theme);
+			
+			Debug.Trace("Host Game State has been entered!");
+			
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+			Debug.Trace(e.getMessage());
+		}
+		
 	}
 
 	@Override
