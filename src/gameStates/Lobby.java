dbcec77bc6b15827c2ed398d2fe83d9870f3e956
zerodivided714 <zerodivided714@localhost>
2013-10-31 17:41:30 -0400

    Added the Lobby Gamestate

---
+++ src/gameStates/Lobby.java
@@ -0,0 +1,59 @@
+package gameStates;
+
+import core.stateManager.GameState;
+import core.stateManager.StateManager;
+
+public class Lobby extends GameState {
+
+	public Lobby(StateManager sm) {
+		super(sm);
+		
+	}
+
+	public Lobby() {
+		
+	}
+
+	@Override
+	public void update(int delta) {
+		
+
+	}
+
+	@Override
+	public void draw() {
+		
+
+	}
+
+	@Override
+	public void resume() {
+		
+
+	}
+
+	@Override
+	public void pause() {
+		
+
+	}
+
+	@Override
+	public void save() {
+		
+
+	}
+
+	@Override
+	public void enter() {
+		
+
+	}
+
+	@Override
+	public void exit() {
+		
+
+	}
+
+}
