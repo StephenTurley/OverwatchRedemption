e7b04b830a6fa11214bf4e9ca44c809249e89b86
Stephen Turley <turley714@hotmail.com>
2013-12-03 13:36:50 -0500

    added some licensing stuff

--- src/core/stateManager/StateManager.java
+++ src/core/stateManager/StateManager.java
@@ -1,3 +1,9 @@
+/*******************************************************************************
+ * Copyright 2013 Stephen Turley
+ * 
+ * This software is part of the Overwatch-Redemption and is not licensed for redistribution. 
+ * You may not reproduce any part of this work unless otherwise stated.
+ ******************************************************************************/
 package core.stateManager;
 
 import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
