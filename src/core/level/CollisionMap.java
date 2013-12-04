62eccb512f851af7cc9c7d637b586985e338ba0c
Stephen Turley <turley714@hotmail.com>
2013-12-03 20:34:14 -0500

    Added some of the classes the game will need to manage Level data.

---
+++ src/core/level/CollisionMap.java
@@ -0,0 +1,11 @@
+/*******************************************************************************
+ * Copyright 2013 Stephen Turley
+ * 
+ * This software is part of the Overwatch-Redemption and is not licensed for redistribution. 
+ * You may not reproduce any part of this work unless otherwise stated.
+ ******************************************************************************/
+package core.level;
+
+public class CollisionMap {
+
+}
