4bbc1be307abe1a3c71cbd7ce3158ff96ec3dbac
Stephen Turley <turley714@hotmail.com>
2013-12-22 13:21:37 -0500

    Added copyrights

--- src/core/graphics/Camera.java
+++ src/core/graphics/Camera.java
@@ -1,3 +1,9 @@
+/*******************************************************************************
+ * Copyright 2013 Stephen Turley
+ * 
+ * This software is part of the Overwatch-Redemption and is not licensed for redistribution. 
+ * You may not reproduce any part of this work unless otherwise stated.
+ ******************************************************************************/
 package core.graphics;
 
 import org.lwjgl.util.Point;
--- src/gameStates/LevelLoading.java
+++ src/gameStates/LevelLoading.java
@@ -1,3 +1,9 @@
+/*******************************************************************************
+ * Copyright 2013 Stephen Turley
+ * 
+ * This software is part of the Overwatch-Redemption and is not licensed for redistribution. 
+ * You may not reproduce any part of this work unless otherwise stated.
+ ******************************************************************************/
 package gameStates;
 
 import com.esotericsoftware.kryonet.Connection;
