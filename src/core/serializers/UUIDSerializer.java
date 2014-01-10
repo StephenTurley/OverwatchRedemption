c65702c2c0b3da4c2a81749eb2e87621cd0fd96b
Stephen Turley <turley714@hotmail.com>
2014-01-10 00:02:11 -0500

    Add boilerplate legal crap

--- src/core/serializers/UUIDSerializer.java
+++ src/core/serializers/UUIDSerializer.java
@@ -1,3 +1,9 @@
+/*******************************************************************************
+ * Copyright 2014 Stephen Turley
+ * 
+ * This software is part of the Overwatch-Redemption and is not licensed for redistribution. 
+ * You may not reproduce any part of this work unless otherwise stated.
+ ******************************************************************************/
 package core.serializers;
 
 import com.esotericsoftware.kryo.Kryo;
