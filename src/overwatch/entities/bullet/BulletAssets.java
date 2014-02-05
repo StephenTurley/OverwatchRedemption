5ff8d85ae8c70ac1593524ce47d79e140a887b4e
Stephen Turley <turley714@hotmail.com>
2014-02-04 20:20:30 -0500

    added package and classes for bullet

---
+++ src/overwatch/entities/bullet/BulletAssets.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package overwatch.entities.bullet;
+
+import core.entity.EntityAssets;
+import core.entity.EntityState;
+import core.graphics.Camera;
+import org.lwjgl.util.Point;
+
+/**
+ *
+ * @author stephen
+ */
+public class BulletAssets implements EntityAssets
+{
+
+	@Override
+	public void setState(EntityState entityState)
+	{
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	@Override
+	public void draw(Camera camera, Point position)
+	{
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	@Override
+	public void update(int delta)
+	{
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+	
+}
