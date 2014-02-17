74b695501aa3f9c037bc845b96760fdc5906c294
Stephen Turley <turley714@gmail.com>
2014-02-17 17:38:35 -0500

    testing commit

--- src/com/starstuffgames/overwatch/entities/enemies/turret/TurretAssets.java
+++ src/com/starstuffgames/overwatch/entities/enemies/turret/TurretAssets.java
@@ -9,6 +9,10 @@ package com.starstuffgames.overwatch.entities.enemies.turret;
 import com.starstuffgames.core.entity.EntityAssets;
 import com.starstuffgames.core.entity.EntityState;
 import com.starstuffgames.core.graphics.Camera;
+import com.starstuffgames.core.graphics.SpriteLoader;
+import com.starstuffgames.core.graphics.StaticSpriteSheet;
+import com.starstuffgames.overwatch.cardinality.Direction;
+import com.starstuffgames.overwatch.cardinality.DirectionMap;
 import org.lwjgl.util.Point;
 
 /**
@@ -17,6 +21,17 @@ import org.lwjgl.util.Point;
  */
 public class TurretAssets implements EntityAssets
 {
+    private final DirectionMap directionMap;
+    private Direction currentDirection;
+    private EntityState currentState;
+    
+    public TurretAssets(EntityState startingEntityState) throws Exception
+    {
+        StaticSpriteSheet spriteSheet = SpriteLoader.load("/spriteSheetData/Turret.xml");
+        directionMap = new DirectionMap(spriteSheet, startingEntityState);
+        currentDirection = Direction.N;
+        currentState = startingEntityState;
+    }
 
 	@Override
 	public void setState(EntityState entityState)
