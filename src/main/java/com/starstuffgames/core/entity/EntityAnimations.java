1c2e029a7c2f8bd7a45389e16ae139461c5e26d0
Stpehen <turley714@gmail.com>
2014-05-19 21:44:38 -0400

    Removed hacked in netbeans support from gradle.build Fixed warning in EntityAnimations.java

--- src/main/java/com/starstuffgames/core/entity/EntityAnimations.java
+++ src/main/java/com/starstuffgames/core/entity/EntityAnimations.java
@@ -1,10 +1,8 @@
 package com.starstuffgames.core.entity;
 
-import com.starstuffgames.core.graphics.cardinality.DirectionMap;
 
 public class EntityAnimations {
-	
-	private DirectionMap directionMap;
+
 	
 	public EntityAnimations(String animationSheetPath, EntityState entityState)
 	{
